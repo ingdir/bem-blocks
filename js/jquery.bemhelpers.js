7efffb84ece0fac4c0c8314c58e31665c1c34016
max.shirshin <max.shirshin@2r-media.de>
2014-02-11 18:29:37 +0100

    .onSetMod() implemented as a way to hook into modifier changes

--- js/jquery.bemhelpers.js
+++ js/jquery.bemhelpers.js
@@ -1,16 +1,38 @@
+/**
+ * jQuery plugin for basic BEM manipulations.
+ * 
+ * @author Max Shirshin
+ * @version 2.0.0
+ * 
+ */
 (function($, undefined) {
+    
+    var bemModMap = 'bemHelpers:modMap',  // unique key for jQuery data storage
+        // kind of a hash function to store callbacks
+        // for a specific block/element/modifier combo
+        getMapKey = function(block, elem, modName, modVal) {
+            return block + (elem !== undefined ? '__' + elem : '') + '_' + modName + '_' + (typeof modVal === 'boolean' ? '*' : (modVal || '*')); 
+        },
+        getElemClasses = function(domEl) {
+            if (domEl.classList) {
+                return domEl.classList;
+            } else {
+                return $.trim(domEl.className).split(/\s+/);
+            }
+        };
     
     $.extend($.fn, {
         getMod: function(block, elem, modName) {
             if (modName === undefined) {
                 modName = elem;
                 elem = undefined;
-            } 
+            }
+            
             if (this.length) {
                 var classPattern = block + (elem !== undefined ? '__' + elem : '') + '_' + modName,
                     modVal = false;
                 
-                $.each(this.get(0).classList, function(i, c) {
+                $.each(getElemClasses(this.get(0)), function(i, c) {
                     if (c === classPattern) {
                         modVal = true;
                         return false;
@@ -41,18 +63,63 @@
                 } else if (modVal === true) {
                     $this.addClass(classPattern);
                 } else {
-                    var condemned = [];
-                    $.each(this.classList, function(i, c) {
+                    $.each(getElemClasses(this), function(i, c) {
                         if (c.indexOf(classPattern) === 0 && c.substr(classPattern.length, 1) === '_') {
-                            condemned.push(c);
+                            $this.removeClass(c);
                         }
-                    });
-                    $.each(condemned, function(i, c) {
-                        $this.removeClass(c);
                     });
                     
                     $this.addClass(classPattern + '_' + modVal);
                 }
+                
+                // the actions are run AFTER the class name is set
+                var modMap = $this.data(bemModMap);
+                if (modMap) {
+                    // universal callbacks for all modifier values '*' 
+                    var fnGlobalArrayRef = modMap[getMapKey(block, elem, modName)] || [],
+                        // specific values    
+                        fnArrayRef = modMap[getMapKey(block, elem, modName, modVal)] || [];
+
+                    $.each(fnGlobalArrayRef.concat(fnArrayRef), function(i, fn) {
+                        // "this" is set to a jQuery object representing the current element
+                        fn.call($this, modName, modVal, block, elem);
+                    });
+                }
+            });
+        },
+        
+        onSetMod: function(block, elem, modMap) {
+            if (modMap === undefined) {
+                modMap = elem;
+                elem = undefined;
+            }
+            
+            return this.each(function() {
+                var $this = $(this),
+                    exModMap = $this.data(bemModMap);
+                
+                if (typeof exModMap !== 'object') {
+                    exModMap = {};
+                    $this.data(bemModMap, exModMap);
+                }
+                
+                $.each(modMap, function(modName, val) {
+                    var key;
+                    if ($.isFunction(val)) {
+                        key = getMapKey(block, elem, modName);
+                        exModMap[key] = exModMap[key] || [];
+                        exModMap[key].push(val);
+                    } else if (typeof val === 'object') {
+                        $.each(val, function(modVal, fn) {
+                            if ($.isFunction(fn)) {
+                                key = getMapKey(block, elem, modName, modVal);
+
+                                exModMap[key] = exModMap[key] || [];
+                                exModMap[key].push(fn);
+                            }
+                        });
+                    }
+                });
             });
         }
     });
